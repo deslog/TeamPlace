1fbaaf68d93388c4b18697f900c8113d238fd4ee
JI SOO <dlwltn815@gmail.com>
2022-04-30 18:29:34 +0900

    Initial Commit

---
+++ TeamPlace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeamPlace
+//
+//  Created by LeeJiSoo on 2022/04/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TeamPlace/TeamPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamPlaceApp.swift
+//  TeamPlace
+//
+//  Created by LeeJiSoo on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct TeamPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
