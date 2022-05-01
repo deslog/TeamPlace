3a7e506e01dcaee59878dca1ea0a6c178c1e3ea0
JI SOO <dlwltn815@gmail.com>
2022-05-02 00:39:04 +0900

    PlaceInfoView 생성
    - PlaceInfoView 임시 뷰 생성

---
+++ TeamPlace/PlaceInfoView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct PlaceInfoView: View {
+//    @Binding var giveImage: String
+    
+    var body: some View {
+        Text("여기는 PlaceInfoView 페이지")
+    }
+}
+
+
+//
+//
+//struct PlaceInfoView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PlaceInfoView(giveImage: .constant(String))
+//    }
+//}
