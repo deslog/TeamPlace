1008dae879a2b27997a8615823dc6629c269783e
JI SOO <dlwltn815@gmail.com>
2022-05-01 22:31:47 +0900

    MainScroll 분리
    - mainscroll 분리

---
+++ TeamPlace/MainScroll.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct MainScroll: View {
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Text("My Place")
+                    .bold()
+                    .font(.system(size: 25))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(EdgeInsets(top: 16, leading: 16, bottom: 5, trailing: 16))
+                MyPlaceList()
+                    .frame(height: 200)
+                Text("Team Place")
+                    .bold()
+                    .font(.system(size: 25))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(EdgeInsets(top: 16, leading: 16, bottom: 5, trailing: 16))
+                PlaceList()
+            }
+        }
+    }
+}
+
+struct MainScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScroll()
+    }
+}
--- TeamPlace/MainView.swift
+++ TeamPlace/MainView.swift
@@ -3,28 +3,14 @@ import SwiftUI
 struct MainView: View {
     
     var body: some View {
-        ScrollView {
-            VStack {
-                Text("My Place")
-                    .bold()
-                    .font(.system(size: 25))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(EdgeInsets(top: 16, leading: 16, bottom: 5, trailing: 16))
-                MyPlaceList()
-                    .frame(height: 200)
-                Text("Team Place")
-                    .bold()
-                    .font(.system(size: 25))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(EdgeInsets(top: 16, leading: 16, bottom: 5, trailing: 16))
-                PlaceList()
-            }
-        }
+        MainScroll()
     }
-    
-    struct MainView_Previews: PreviewProvider {
-        static var previews: some View {
-            MainView()
-        }
+}
+
+
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
     }
 }
