dca8462f7f286a7f0b5cb346df1153b77c68b041
JI SOO <dlwltn815@gmail.com>
2022-05-02 00:39:33 +0900

    AddPlaceView 생성
    - AddPlaceView 임시 뷰 생성

---
+++ TeamPlace/AddPlaceView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+
+
+struct AddPlaceView: View {
+    
+    var body: some View {
+        Text("여기는 addplace 페이지")
+    }
+}
+
+
+
+
+struct AddPlaceView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPlaceView()
+    }
+}
