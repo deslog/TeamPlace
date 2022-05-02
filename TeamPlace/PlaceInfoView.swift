58faeab3284aeed5e00f4887755f863074b61218
JI SOO <dlwltn815@gmail.com>
2022-05-02 17:04:52 +0900

    Update PlaceInfoView.swift
    -PlaceInfoView Info rectangle 위치 Vstack spacing 으로 수정

--- TeamPlace/PlaceInfoView.swift
+++ TeamPlace/PlaceInfoView.swift
@@ -1,24 +1,44 @@
 import SwiftUI
 
 struct PlaceInfoView: View {
-    @State var giveImage: String
+    @State var infoViewImage: String
+    @State var infoViewName: String
+    @State var infoViewAddress: String
+    
+    var infoRectangle: some View {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 300, height: 170, alignment: .center)
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.gray.opacity(0.2), radius: 1, x: 3, y: 3)
+                    .shadow(color: Color.gray.opacity(0.2), radius: 1, x: -3, y: -3)
+                Text(infoViewName)
+                    .bold()
+                    .foregroundColor(Color.brown)
+            }
+    }
+    
     
     var body: some View {
-        VStack{
-            Text("여기는 PlaceInfoView 페이지")
-            Image(giveImage)
-                .resizable()
-                .frame(width: 200, height: 200, alignment: .center)
+        VStack {
+            VStack(spacing: -190){
+                Image(infoViewImage)
+                    .resizable()
+                    .frame(maxWidth: .infinity, maxHeight: 290, alignment: .topLeading)
+                    .ignoresSafeArea()
+                infoRectangle
+            }
+            Spacer()
         }
-
     }
 }
 
 
-//
-//
-//struct PlaceInfoView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PlaceInfoView(giveImage: .constant(String))
-//    }
-//}
+
+
+struct PlaceInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaceInfoView(infoViewImage: "cafe2", infoViewName: "이름이뭐예요", infoViewAddress: "남구 12134")
+            .previewInterfaceOrientation(.portraitUpsideDown)
+    }
+}
