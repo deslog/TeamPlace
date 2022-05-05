e65856e4386295043c5b147f788837acc65b289e
JI SOO <dlwltn815@gmail.com>
2022-05-05 23:39:15 +0900

    Feat: ReviewModal
    - 리뷰모달 생성

--- TeamPlace/TeamPlaceApp.swift
+++ TeamPlace/TeamPlaceApp.swift
@@ -5,7 +5,8 @@ struct TeamPlaceApp: App {
     var body: some Scene {
         WindowGroup {
             //프로젝트가 시작될 때, environmentobject로 밖에서 userVM를 받아버린다!
-            LoginMain().environmentObject(UserVM())
+//            LoginMain().environmentObject(UserVM())
+            MainView()
         }
     }
 }
